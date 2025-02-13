e4f30aeea137d5a05a636e0fabc01ed48dfbe66a
Morgado, Miguel (contracted) <Miguel.Morgado@contracted.pmi.com>
2025-02-13 16:41:53 +0000

    try-ignore-inside-api

--- api_e2e/dist/env/parseEnv.js
+++ api_e2e/dist/env/parseEnv.js
@@ -4,15 +4,13 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.getJsonFromFile = exports.env = void 0;
-const env = key => {
-  const value = process.env[key];
+var env = exports.env = function env(key) {
+  var value = process.env[key];
   if (!value) {
     throw Error("\uD83E\uDDE8 No environment variable found for ".concat(key, " \uD83E\uDDE8"));
   }
   return value;
 };
-exports.env = env;
-const getJsonFromFile = path => {
+var getJsonFromFile = exports.getJsonFromFile = function getJsonFromFile(path) {
   return require("".concat(process.cwd()).concat(path));
 };
-exports.getJsonFromFile = getJsonFromFile;
--- api_e2e/dist/support/host-helper.js
+++ api_e2e/dist/support/host-helper.js
@@ -4,15 +4,11 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 exports.retrieveHostURL = void 0;
-const retrieveHostURL = _ref => {
-  let {
-    hostsConfig
-  } = _ref;
-  const {
-    API_AUTOMATION_HOST: hostname = 'production'
-  } = process.env;
-  const hostPath = hostsConfig[hostname];
-  const url = new URL(hostPath);
+var retrieveHostURL = exports.retrieveHostURL = function retrieveHostURL(_ref) {
+  var hostsConfig = _ref.hostsConfig;
+  var _process$env$API_AUTO = process.env.API_AUTOMATION_HOST,
+    hostname = _process$env$API_AUTO === void 0 ? 'production' : _process$env$API_AUTO;
+  var hostPath = hostsConfig[hostname];
+  var url = new URL(hostPath);
   return url;
 };
-exports.retrieveHostURL = retrieveHostURL;
